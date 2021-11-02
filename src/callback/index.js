61e795e21809a0b696e63d0353957c9d88bc64e8
root <root@andrey-02.localdomain>
2021-11-02 17:22:50 -0500

    gitignore

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,12 +1,12 @@
-// function sum(num1, num2) {
-//     return num1 + num2
-// }
+function sum(num1, num2) {
+    return num1 + num2
+}
 
-// function cal(num1, num2, callback) {
-//     return callback(num1, num2)
-// }
+function cal(num1, num2, callback) {
+    return callback(num1, num2)
+}
 
-// console.log(cal(6, 2, sum))
+console.log(cal(6, 2, sum))
 
 function date (callback){
     console.log(new Date)
